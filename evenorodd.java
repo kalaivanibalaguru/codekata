f7d930d1420a6d443fc67746e43dfed12e6d1d57
kalaivanibalaguru <kalaivanibalaguru1@gmail.com>
2017-07-09 14:05:31 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class evenorodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num;
+		Scanner s=new scanner(System.in);
+	    System.out.println("input-number: ");
+	    num=s.nextInt();
+		if(num%2==0)
+		{System.out.println("The number is even.");}
+		else 
+		{System.out.println("The number is odd");}
+	 
+	}
+}
